6eaeeddb1f89a97b4a94a88cbcd776a93a2039a5
chen <294225707@qq.com>
2018-07-02 13:28:42 +0800

    删除 pyc

--- MxOline/settings.py
+++ MxOline/settings.py
@@ -94,9 +94,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mxonline',
         'HOST': '127.0.0.1',
-        'PORT': '8889',
+        'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
     }
 }
 
